cdbfbacbfbdb2e217c9740f7889a0a12bbe932bb
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-08-09 09:13:11 +0530

    solution of my 1st problem

---
+++ 1.problem-solution/01_problem.py
@@ -0,0 +1,16 @@
+# AGE GROUP CATEGORIZATION 
+
+age=int(input("Provide me a age :"))# we should convert into int
+
+if age<13:
+        print("He/She is a child !")
+
+elif age<20:
+        print("He/She is a TeenAger !")
+
+elif age<59:
+        print("He/She is a Adult !")
+
+else:
+        print("He/She is a senior !")
+
