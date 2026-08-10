02f32415b079a2f26620af950442f81577154a5a
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-08-10 12:40:26 +0530

    find the factorial

--- 2.problem-solution-loops/6.factorial_calcuator.py
+++ 2.problem-solution-loops/6.factorial_calcuator.py
@@ -2,7 +2,8 @@ number=4
 factorial=1
 
 while number>0:
-    factorial =factorial*number
+    # factorial =factorial*number
+    factorial *=number
     number=number-1
     print("The process:",factorial)
 print("The final  is",factorial)  
