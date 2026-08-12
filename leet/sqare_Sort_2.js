2d04a7f4736110391a41510806d9146fea8185b7
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-08-12 22:52:49 +0530

    new method to sort

---
+++ leet/sqare_Sort_2.js
@@ -0,0 +1,21 @@
+// New method to sort
+function sortedSquares(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = new Array(nums.length);
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        let leftSquare = nums[left] * nums[left];
+        let rightSquare = nums[right] * nums[right];
+
+        if (leftSquare > rightSquare) {
+            result[i] = leftSquare;
+            left++;
+        } else {
+            result[i] = rightSquare;
+            right--;
+        }
+    }
+
+    return result;
+}
