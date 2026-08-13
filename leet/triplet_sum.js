e3abac8defad6f5f538abcbd0d6ff1bd6964af8e
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-08-13 21:53:29 +0530

    solution of triplet sum

---
+++ leet/triplet_sum.js
@@ -0,0 +1,47 @@
+// Approach one
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    let result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        // Duplicate first element skip karo
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+
+            let sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                left++;
+                right--;
+
+                // Duplicate left values skip karo
+                while (left < right && nums[left] === nums[left - 1]) {
+                    left++;
+                }
+
+                // Duplicate right values skip karo
+                while (left < right && nums[right] === nums[right + 1]) {
+                    right--;
+                }
+
+            } else if (sum < 0) {
+                left++;
+
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+};
