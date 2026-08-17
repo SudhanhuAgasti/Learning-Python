ec1a2780c9516cec54acb2357b429b2cc1bac998
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-08-17 23:00:47 +0530

    some newly modified code

--- 4.oop/aolution.py
+++ 4.oop/aolution.py
@@ -1,14 +1,17 @@
 class Car:
     def __init__(self,brand,model):
         # here __init__ is constructor in the python
-        self.brand= brand
+        self.__brand= brand
         self.model= model
+
+    def get_brand(self):
+        return self.__brand + " !"    
 
     def fullName(self):
         # self.istname=istname
         # self.last_name=last_name
-        return f"{self.brand}{self.model}"
-
+        return f"{self.__brand}{self.model}"
+# in heritence ------------>
 class ElectricCar(Car):# inherit from the car class 
         def __init__(self, brand,model,battery_size):
             super().__init__(brand,model)
@@ -17,9 +20,12 @@ class ElectricCar(Car):# inherit from the car class
 
 
 my_car= Car("Toyata","Corola")   
-print("The brand of the car is : ",my_car.brand)
-print("The model of the car is :",my_car.model)
-print(my_car.fullName())
+# print("The brand of the car is : ",my_car.brand)
+# print("The model of the car is :",my_car.model)
+# print(my_car.fullName())
 
 my_tesla= ElectricCar("Tesla","model S","85kwh")
-print(my_tesla.model( ))
+print(my_tesla.fullName())
+print(my_tesla.model)
+print(my_tesla.__brand)
+print(my_tesla.get_brand())
