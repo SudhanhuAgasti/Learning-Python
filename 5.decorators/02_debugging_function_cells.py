b1e8ce98668544b7600fb980237cffc0a9078d46
SUDHANSHU KUMAR AGASTI <sudhanshuagasti588@gmail.com>
2026-09-18 17:53:27 +0530

    debugging function

---
+++ 5.decorators/02_debugging_function_cells.py
@@ -0,0 +1,21 @@
+# QN> create a decorateor to print the function name and value of its arguments everytime the function is called .
+# now make the toolbooth
+
+def debug(func):
+    def wrapper(*args,**kwargs):
+        args_value=', '.join(str(arg) for arg in args)
+        kwargs_value=', '.join( f"{k}= {v}" for k,v in kwargs.items())
+        print(f"calling:{func.__name__} with args {args_value} and kwargs {kwargs_value}")
+        return func(*args,**kwargs)
+    return wrapper # it actually returns defination.
+
+
+
+
+
+
+@debug
+def greet(name,greeting="Hello"):
+    print(f"{greeting}, {name}")
+
+greet("chai!", greeting="hn ji")    
